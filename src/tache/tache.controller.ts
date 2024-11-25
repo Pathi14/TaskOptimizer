01ef2c3071be7e12e3daa9c4a31ed3eaef026bdd
pathi14 <pathipayong@gmail.com>
2024-11-25 16:02:47 +0100

    feat : CRUD tache MVP - OK

--- src/tache/tache.controller.ts
+++ src/tache/tache.controller.ts
@@ -1,21 +1,34 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { TacheService } from './tache.service';
+import { Tache } from '@prisma/client';
 
 @Controller()
 export class TacheController {
     constructor(private readonly tacheService: TacheService) {}
 
     @Post()
-    async createTask(
+    async createTache(
     @Body() body: { titre: string; description: string; date_echeance: Date; priorité: number; }
     ) {
         const { titre, description, date_echeance, priorité } = body;
         return this.tacheService.addTache(titre, description, date_echeance, priorité);
     }
 
-
     @Get()
-    getTache(): string {
-        return this.tacheService.getTache();
+    async getTaches(): Promise<Tache[]> {
+        return this.tacheService.getTaches();
+    }
+    
+    @Put(':id')
+    async updateTache(
+        @Param('id') id: number,
+        @Body() body: { titre?: string; description?: string; date_echeance?: Date; priorite?: number },
+    ): Promise<Tache> {
+        return this.tacheService.updateTache(id, body);
+    }
+
+    @Delete(':id')
+    async deleteTache(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.tacheService.deleteTache(id);
     }
 }
--- src/tache/tache.service.ts
+++ src/tache/tache.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Tache } from '@prisma/client';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
 
 @Injectable()
 export class TacheService {
-
+    
     constructor(private prisma: PrismaService) {}
-
+    
     async addTache(titre: string, description: string, date_echeance:Date, priorite: number): Promise<void> {
         await this.prisma.tache.create({
             data: {
                 titre,
                 description: description,
-                date_echeance,
+                date_echeance: date_echeance,
                 priorite,  
                 projetId: null,
                 statutId: null,
@@ -19,8 +20,21 @@ export class TacheService {
             },
         });
     }
+    
+    async updateTache(id: number, data: { titre?: string; description?: string; date_echeance?: Date; priorite?: number }): Promise<Tache> {
+        return this.prisma.tache.update({
+            where: { id }, 
+            data,
+        });
+    }
+    
+    async getTaches(): Promise<Tache[]> {
+        return this.prisma.tache.findMany();
+    }
 
-    getTache(): string {
-        return 'tache créé!';
+    async deleteTache(id: number): Promise<void>{
+        await this.prisma.tache.delete({
+            where: {id},
+        });
     }
 }
